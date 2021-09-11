0d931adbb18ee2d13017fc838bd8a01fa926e4b8
Sebastian Ruiz <ruizju@javeriana.edu.co>
2021-09-11 12:36:56 -0500

    Intento 2 servidor

--- Taller RMI/RegistradorClient.java
+++ Taller RMI/RegistradorClient.java
@@ -1,4 +1,6 @@
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 /**
  *
  * @author usuario
@@ -8,8 +10,13 @@ public class RegistradorClient {
     public static void main(String args[]) {
 
         try {
+            String serverIP = "25.12.51.131"; // or localhost if client and server on same machine.
+            int serverPort = 1099;
+            Registry registry = LocateRegistry.getRegistry(serverIP, serverPort);
+
+            Registrador miregistrador = (Registrador) registry.lookup("MiRegistrador");
             System.out.println("Buscando Objeto ");
-            Registrador miregistrador = (Registrador) Naming.lookup("rmi://" + "25.12.51.131:1099" + "/" + "MiRegistrador");
+            // Registrador miregistrador = (Registrador) Naming.lookup("rmi://" + "25.12.51.131:1099" + "/" + "MiRegistrador");
 
             // System.out.println("Pudo");
 
